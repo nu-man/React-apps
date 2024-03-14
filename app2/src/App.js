1e7f2bc4294a55b8b0faee08b7d7d13d62739bb9
nu-man <mohdnuman198@gmail.com>
2024-03-14 17:12:13 +0530

    fetched data using useState and useEffect

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState,useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(()=>{
+    let getdata = async () => {
+      try {
+        const res = await axios.get(`https://jsonplaceholder.typicode.com/todos`);
+        setData(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getdata()
+  },[])
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <center>
+        <button>Task Status </button>
+        <table>
+          <thead>
+            <tr>
+              <th>UserId</th>
+              <th>Id</th>
+              <th>Title</th>
+              <th>Completed</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((obj) => (
+
+            
+              <tr key={obj.id}>
+                <td>{obj.id}</td>
+                <td>{obj.userId}</td>
+
+                <td>{obj.title}</td>
+                <td>{obj.completed ? "True" : "False"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </center>
+    </>
   );
 }
-
 export default App;
